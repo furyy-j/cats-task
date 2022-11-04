01584cc62c3cb6c543bf85c5ed00ed231170c49f
furyy-j <96385920+furyy-j@users.noreply.github.com>
2022-11-04 21:43:42 +0200

    created spinner loader module

---
+++ src/app/modules/spinner/spinner.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { SpinnerComponent } from './spinner/spinner.component';
+import { CommonImportsModule } from '../common-imports.module';
+
+
+
+@NgModule({
+    declarations: [
+        SpinnerComponent
+    ],
+    imports: [
+        CommonImportsModule
+    ],
+    exports: [
+        SpinnerComponent,
+    ]
+})
+export class SpinnerModule { }
